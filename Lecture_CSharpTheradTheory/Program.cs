e19611da5be4af72264dd3ebc388fd84b83a82ad
KrumiChan <lovelybarry@naver.com>
2021-08-22 19:18:03 +0900

    First Initialization.

---
+++ Lecture_CSharpTheradTheory/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lecture_CSharpTheradTheory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
