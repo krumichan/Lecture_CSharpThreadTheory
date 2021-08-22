790e0a3a6bf676d02b00b21cfe1b012266a2f03b
KrumiChan <lovelybarry@naver.com>
2021-08-22 19:39:24 +0900

    Thread Local Storage 이론 공부.

---
+++ Lecture_CSharpTheradTheory/ThreadTheory_009_TransportLayerSecurity.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Lecture_CSharpTheradTheory
+{
+    class ThreadTheory_009_TransportLayerSecurity
+    {
+        // Thread 고유의 전역 변수.
+        static ThreadLocal<string> ThreadName = new ThreadLocal<string>(() => { return $"My Name Is {Thread.CurrentThread.ManagedThreadId}"; });
+
+        void WhoAmI()
+        {
+            /*ThreadName.Value = $"My Name Is {Thread.CurrentThread.ManagedThreadId}";
+            Thread.Sleep(1000);
+            Console.WriteLine(ThreadName.Value);*/
+
+            bool repeat = ThreadName.IsValueCreated;
+            if (repeat)
+            {
+                Console.WriteLine(ThreadName.Value + "(repeat)");
+            }
+            else
+            {
+                Console.WriteLine(ThreadName.Value);
+            }
+        }
+
+        public void Execute()
+        {
+            ThreadPool.SetMinThreads(1, 1);
+            ThreadPool.SetMaxThreads(3, 3);
+            Parallel.Invoke(WhoAmI, WhoAmI, WhoAmI, WhoAmI, WhoAmI, WhoAmI, WhoAmI, WhoAmI);
+
+            ThreadName.Dispose();
+        }
+    }
+}
